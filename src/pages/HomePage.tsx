712415184a3dbcfb7be2cab0055da20c4f6fc70e
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-11-06 18:22:34 -0500

    refactor(pages): improve layout and responsiveness in HomePage
    adjust image container height in EstudioPage and add HomePage to gitignore

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,24 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
-
 export default function HomePage() {
   return (
-    <div className="relative flex min-h-screen w-full flex-col">
-      <Header />
-       <main className="flex flex-1 items-center justify-center">
-        <div
-          className="bg-cover bg-center rounded-2xl shadow-xl flex items-center justify-center"
-          style={{
-            backgroundImage: "url('/foto_main.jpg')",
-            width: "70vw",
-            height: "60vh",
-          }}
-        >
-          
+    <div className="relative flex min-h-screen w-full flex-col bg-background-light dark:bg-background-dark">
+      <div className="flex-1 flex items-center justify-center px-4 sm:px-8 md:px-16 lg:px-24 xl:px-40 py-5">
+        <div className="w-full max-w-[1280px] mx-auto">
+          <Header />
+          <main className="flex items-center justify-center mt-20">
+            <div
+              className="bg-cover bg-center rounded-2xl shadow-xl flex items-center justify-center w-full max-w-5xl"
+              style={{
+                backgroundImage: "url('/foto_main.jpg')",
+                height: "60vh",
+              }}
+            >
+            </div>
+          </main>
         </div>
-      </main>
+      </div>
       <Footer />
     </div>
   );
